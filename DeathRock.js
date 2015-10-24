21a5a8966798f2d8cf2a08c30c24ff247b41aa3d
iuyhcdfs <iuyhcdfs@gmail.com>
2015-10-25 00:31:38 +1100

    mechanics, but oh no thats not how you draw :(

--- DeathRock.js
+++ DeathRock.js
@@ -1,55 +1,59 @@
 // SIT DOWN AND LEAN PREFERABLY ON YOUR CHAIR: DEATH ROCKIN STRAIGHT TO YOUR THEMATIC CONCLUSION OF NOT LIVING
 
-// With some starting code from Daniel Albu's "Pong Game" create.js tutorial
+var canvas, stage; 
 
-// can we get this by canvas id?
-var CANVAS_WIDTH = 720;
-var CANVAS_HEIGHT = 830;
-
-// the only static level variable. implement antiGround for broken terrain later
-var groundHeight = 0;
-
-// the top level objects
-var canvas;
-var stage; 
-var player = new RockChair;
+var w, h;
+var groundHeight;
 
 // to load graphics assets
-var img_player; 
-var img_sky; 
-var img_title;
+var img_player, img_sky, img_title;
+
+var player;
 
 function init() {
     stage = new createjs.Stage("DeathRockStage");
     stage.mouseEventsEnabled = true;
+    w = stage.canvas.width;
+    h = stage.canvas.height;
     
     // test circle
     var circle = new createjs.Shape();
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 10);
     circle.x = 100;
     circle.y = 100;
     stage.addChild(circle);
     stage.update();
     
     // images
-    /*
     manifest = [
-                {src:'player.png', id:'img_player'},
-                {src:'sky.png', id:'img_sky'},
-                {src:'title.png', id:'img_title'}
-                ];
-    preloader = new PreloadJS();
-    preloader.onProgress = handleProgress;
-    preloader.onComplete = handleComplete;
-    preloader.onFileLoad = handleFileLoad;
-    preloader.loadManifest(manifest);
-    */
+        {src:'player.png', id:'img_player'},
+        {src:'sky.png', id:'img_sky'},
+        {src:'title.png', id:'img_title'}
+    ];
+    loader = new createjs.LoadQueue(false);
+    loader.addEventListener("complete", handleComplete);
+    loader.loadManifest(manifest, true, ".");
+    
+    RockChair.testergh();
+    
     // ticker does timestep
     Ticker.setFPS(60);
-    Ticker.addListener(stage);
+    Ticker.addEventListener("tick", tick);
     
 }
 
+function handleComplete(){
+    img_player = new Shape();
+    img_player.graphics.beginBitmapFill(loader.getResult("img_player")).drawRect(0,0,200,200);
+    stage.addChild(img_player);
+    
+    var test = loader.getResult("img_player");
+    player = new createjs.Shape();
+    player.graphics.beginBitmapFill(test).drawRect(0,0,w+100, h+100);
+    
+    
+    stage.update();
+}
 
 // original thing which you dodge
 var Projectile = (function () {
@@ -87,8 +91,9 @@ var Life = (function () {
 var RockChair = (function () {
     
     // positive velocity goes right of course
-    var START_X = CANVAS_WIDTH/2;
-    var START_Y = groundHeight;
+    var START_X = stage.canvas.width / 2;
+    var START_Y = stage.canvas.height;
+    
     var lean = 0;    
     var x = START_X;
     var y = START_Y;
@@ -104,6 +109,16 @@ var RockChair = (function () {
     var LEAN_OVERLEAN = 20;
     var FRICTION = 3;
     var OVERLEAN_MODULO = 10;
+
+    function testergh(){
+            
+        var circle = new createjs.Shape();
+        circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 100);
+        circle.x = 10;
+        circle.y = 10;
+        stage.addChild(circle);
+        stage.update();
+    }
 
     function act_left(){
         dx = -10;
@@ -136,47 +151,3 @@ var RockChair = (function () {
     }
 
 })()
-
-/*
-// file loading
-
-function handleProgress(event)
-{
-    //use event.loaded to get the percentage of the loading
-}
-
-function handleComplete(event) {
-         
-}
-
-function handleFileLoad(event) {
-
-            
-    switch(event.type)
-    {
-        case PreloadJS.IMAGE:
-        //image loaded
-         var img = new Image();
-         img.src = event.src;
-         img.onload = handleLoadComplete;
-         window[event.id] = new Bitmap(img);
-        break;
-
-        case PreloadJS.SOUND:
-        //sound loaded
-        handleLoadComplete();
-        break;
-    }
-}
-
- function handleLoadComplete(event) 
- {
-
-    totalLoaded++;
-    
-    if(manifest.length==totalLoaded)
-    {
-        addTitleView();
-    }
- }
- */
