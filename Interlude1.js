29d16cf7852e343ab58028cfa24cbf826417fc8c
Fordy <fordy@smartsparrow.com>
2015-10-24 18:42:28 -0700

    master updted interlude

--- Interlude1.js
+++ Interlude1.js
@@ -13,8 +13,17 @@ var Interlude1 = (function () {
         }
         this.i = 0;
         this.colour = '#'+Math.round(Math.random() * 0xFFFFFF).toString(16);
+        this.posX = Math.random() * 600;
+        this.posY = Math.random() * 600;
         this.update = function (stage) {
             this.i += 1;
+
+            var text = new Text("I feel colder", 'bold 20px Arial', '#333333');
+            text.x = 100 + this.posX;
+            text.y = 100 + this.posY + this.i / 30;
+
+            stage.addChild(text);
+            this.displayObjects.push(text);
 
             var shape = new Shape();
             shape.graphics.beginFill(this.colour).drawCircle(0, 0, 100 - Math.cos(this.i / 300 * Math.PI / 2) * 100);
